b9ab6bb82fac2b98eabe43f76e03342b128aa9b6
sai <sai@sais-MacBook-Pro.local>
2017-03-16 21:47:42 +0530

    Initial Commit

---
+++ Clozbz/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Clozbz
+//
+//  Created by sai on 3/16/17.
+//  Copyright © 2017 sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Clozbz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clozbz
+//
+//  Created by sai on 3/16/17.
+//  Copyright © 2017 sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
